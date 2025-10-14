a6ade36cb61a625bd9eebd02f7bc9c1feb097c14
TonyGrif <TonyGriffin2000@gmail.com>
2025-10-14 19:29:08 -0400

    dags(usgs): add sliding time calls to dag

--- dags/usgs_etl.py
+++ dags/usgs_etl.py
@@ -1,14 +1,22 @@
 """This DAG aquires USGS data at regular time intrevals"""
 
+from typing import Dict
 from airflow.sdk import dag, task
 from datetime import datetime, timedelta
+import yaml
 
 from usgs import get_instantaneous_values
 
 
+def read_config(path: str) -> Dict:
+    """Read in configuration file to dictionary"""
+    with open(path, mode="r") as f:
+        return yaml.safe_load(f)
+
+
 @dag(
     "USGS",
-    description="Request USGS instantaneous values",
+    description="ETL pipeline for USGS instantaneous values",
     start_date=datetime(2021, 1, 1),
     schedule=None,
     catchup=False,
@@ -17,12 +25,33 @@ from usgs import get_instantaneous_values
 def usgs_instantaneous_values():
     @task(task_id="extract", retries=3, retry_delay=timedelta(minutes=3))
     def extract():
+        # Check db for instances
+        # If none, get historic data and populate to current
+        # Else, query data from latest in db -> current
+
+        # TODO: configure path location via CLI
+        # TODO: work with volume
+        config = read_config("./config.yaml")
+
+        # Populate with historic data
+        start = datetime.fromisoformat(config["start_date"])
+
+        while (datetime.now() - start).days > 365:
+            end = start.replace(year=start.year + 1)
+            df = get_instantaneous_values(
+                site_id="0204295505",
+                params=["62620", "00045", "00036", "00035"],
+                start_date=start.isoformat(),
+                end_date=end.isoformat(),
+            )
+            print(df)
+
+            start = end
+
         df = get_instantaneous_values(
-            "0204295505",
-            ["62620", "00045", "00036", "00035"],
-            "2022-03-01",
-            "2022-03-18",
-            timeout=10,
+            site_id="0204295505",
+            params=["62620", "00045", "00036", "00035"],
+            start_date=start.isoformat(),
         )
         print(df)
 
