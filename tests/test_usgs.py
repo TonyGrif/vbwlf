beb11987a376b4eab5303127cbcc43c4315fcf83
TonyGrif <TonyGriffin2000@gmail.com>
2025-10-14 13:51:03 -0400

    usgs(parser): add json parsing function for usgs iv

--- tests/test_usgs.py
+++ tests/test_usgs.py
@@ -1,10 +1,46 @@
-from usgs import query_instantaneous_values
+import pytest
+from usgs import query_instantaneous_values, parse_instantaneous_values
+
+import pandas as pd
+
+
+@pytest.fixture(scope="class")
+def iv_response():
+    return query_instantaneous_values(
+        "0204295505", ["62620", "00045", "00036", "00035"], "2022-02-22", "2022-02-23"
+    )
+
+
+@pytest.fixture(scope="class")
+def single_iv_response():
+    return query_instantaneous_values("0204295505", "00045", "2022-02-22", "2022-02-23")
 
 
 class TestUSGS:
-    def test_query_iv(self):
-        res = query_instantaneous_values(
-            "0204295505", ["62620", "00045"], "2022-02-22", "2022-02-23"
-        )
-        assert res.status_code == 200
-        assert res.text is not None
+    def test_query_iv(self, iv_response, single_iv_response):
+        assert iv_response.status_code == 200
+        assert iv_response.text is not None
+
+        assert single_iv_response.status_code == 200
+        assert single_iv_response.text is not None
+
+    def test_parse_iv(self, iv_response, single_iv_response):
+        data = iv_response.json()
+        assert isinstance(data, dict)
+
+        df = parse_instantaneous_values(data)
+        assert isinstance(df.index, pd.DatetimeIndex)
+        codes = ["62620", "00045", "00036", "00035"]
+        assert all(col in df.columns for col in codes)
+        assert len(df["62620"]) != 0
+        assert len(df["00045"]) != 0
+        assert len(df["00036"]) != 0
+        assert len(df["00035"]) != 0
+
+        data = single_iv_response.json()
+        assert isinstance(data, dict)
+
+        df = parse_instantaneous_values(data)
+        assert isinstance(df.index, pd.DatetimeIndex)
+        assert "00045" in df.columns
+        assert len(df["00045"]) != 0
--- usgs/parser.py
+++ usgs/parser.py
@@ -2,11 +2,15 @@
 
 Attributes:
     query_instantaneous_values: Send an HTTP request for a site's instantaneous values
+    parse_instantaneous_values: Parse the JSON response from USGS into a DataFrame
 """
 
 import requests
-from typing import List, Optional
+from typing import Dict, List, Optional
 import logging
+
+import pandas as pd
+import numpy as np
 
 logger = logging.getLogger(__name__)
 
@@ -40,17 +44,75 @@ def query_instantaneous_values(
     """
 
     format = "json"
-    paramstr = ",".join(params)
+
+    if isinstance(params, list):
+        paramstr = ",".join(params)
+    else:
+        paramstr = params
 
     url = f"{_SITE_URL}?sites={site_id}&agencyCd={agency}&parameterCd={paramstr}&format={format}"
 
     if start_date is not None:
         url += f"&startDT={start_date}"
     if end_date is not None:
-        url += f"&endDT={start_date}"
+        url += f"&endDT={end_date}"
 
     logger.info("Making request with url: %s", url)
 
     res = requests.get(url, **kwargs)
     res.raise_for_status()
     return res
+
+
+def parse_instantaneous_values(data: Dict) -> pd.DataFrame:
+    """Parse rdb string containing USGS instantaneous values
+
+    Args:
+        data: Dictionary from a USGS instantaneous value query
+
+    Returns:
+        Pandas DataFrame
+    """
+    disclaimer = data["value"]["queryInfo"].get("note")
+    disclaimer = [val for val in disclaimer if val["title"] == "disclaimer"]
+    if disclaimer:
+        logger.warning("%s", disclaimer[0]["value"])
+
+    site = data["value"]["queryInfo"]["criteria"]["locationParam"]
+
+    dataframes = []
+    empty_vars = []
+
+    ts_data = data["value"]["timeSeries"]
+    for param_val in ts_data:
+        var_code = param_val["variable"]["variableCode"][0]["value"]
+        values = param_val["values"][0]["value"]
+
+        if values == []:
+            logger.error(
+                "No data found for parameter code %s, adding NaN column", var_code
+            )
+            empty_vars.append(var_code)
+            continue
+
+        df = pd.DataFrame(values)
+
+        # TODO: Pull in column renaming here from configuration
+        df = df.rename(columns={"dateTime": "datetime", "value": var_code})
+
+        # NOTE: Dropping qualifiers from dataset
+        df = df.drop(columns=["qualifiers"])
+        # df["qualifiers"] = df["qualifiers"].apply(lambda x: x[0] if x else None)
+
+        df["datetime"] = pd.to_datetime(df["datetime"])
+        df = df.set_index("datetime")
+        dataframes.append(df)
+        logger.debug("Values (%s) aquired from %s", len(df), var_code)
+
+    df = pd.concat(dataframes, axis=1)
+
+    for empty in empty_vars:
+        df[empty] = np.nan
+
+    logger.info("Returning DataFrame (size: %s) from site %s", len(df), site)
+    return df
